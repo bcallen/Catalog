5a5c161dbb2adaff92f46741e5faad39e191dc86
Brian Allen <allenbrc@gmail.com>
2016-03-10 09:01:15 -0800

    in progress updates related to functionality for adding notes directly through site.

--- NotesCatalog/catalog/urls.py
+++ NotesCatalog/catalog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<note_id>[0-9]+)/$', views.detail, name='detail'),
 	url(r'^tags/(?P<tag_name>[ _a-zA-Z0-9]+)/$', views.tags, name='tags'),
-	url(r'^(?P<note_id>[0-9]+)/edit/$', views.edit, name='edit')
-	url(r'^/edit/$', views.edit, name='create')
-	url(r'^/add/$', views.detail, name='add')
+	url(r'^edit/(?P<note_id>[0-9]+)/$', views.edit, name='edit')
+	url(r'^edit/$', views.edit, name='create')
+	url(r'^add/$', views.detail, name='add')
 ]
